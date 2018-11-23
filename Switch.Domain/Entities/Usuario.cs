242bf82550e2f1cc5a14162c7d1410290de51054
Bruno Costa <br.gcosta@hotmail.com>
2018-11-23 10:33:57 -0200

    Criação de Classe Usuario e as variáveis

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Switch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    class Usuario
+    {
+        public int Id { get; private set; }
+        public String Nome { get; set; }
+        public String Sobrenome { get; set; }
+        public String Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public String UrlFoto { get; set; }
+        public SexoEnum Sexo { get; set; }
+    }
+}
